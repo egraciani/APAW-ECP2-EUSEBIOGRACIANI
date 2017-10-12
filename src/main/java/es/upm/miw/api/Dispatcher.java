aa271ea5b610a4148bb71e71d682023a6dc64676
egraciani <31991616+egraciani@users.noreply.github.com>
2017-10-12 19:12:26 +0200

    Adding http package from APAW-api-themes. Created empty Dispatcher with auto-generated methods.

---
+++ src/main/java/es/upm/miw/api/Dispatcher.java
@@ -0,0 +1,33 @@
+package es.upm.miw.api;
+
+import es.upm.miw.http.HttpRequest;
+import es.upm.miw.http.HttpResponse;
+
+public class Dispatcher {
+
+    public void doPut(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doGet(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doPost(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doPatch(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public void doDelete(HttpRequest request, HttpResponse response) {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
