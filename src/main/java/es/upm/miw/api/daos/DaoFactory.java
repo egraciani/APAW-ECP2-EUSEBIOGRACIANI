52b9a1b2b00e2d8f51f8d340c1c05598800c379f
egraciani <31991616+egraciani@users.noreply.github.com>
2017-10-12 20:47:42 +0200

    Adding DAOs

---
+++ src/main/java/es/upm/miw/api/daos/DaoFactory.java
@@ -0,0 +1,20 @@
+package es.upm.miw.api.daos;
+
+public abstract class DaoFactory {
+
+    private static DaoFactory factory = null;
+
+    public static void setFactory(DaoFactory factory) {
+        DaoFactory.factory = factory;
+    }
+
+    public static DaoFactory getFactory() {
+        assert factory != null;
+        return factory;
+    }
+
+    public abstract HotelDao getThemeDao();
+
+    public abstract ReservationDao getVoteDao();
+
+}
---
+++ src/main/java/es/upm/miw/api/daos/ReservationDao.java
@@ -0,0 +1,10 @@
+package es.upm.miw.api.daos;
+
+import java.util.List;
+
+import es.upm.miw.api.entities.Reservation;
+
+public interface ReservationDao extends GenericDao<Reservation, Integer> {
+
+    List<Integer> findValueByThemeId(int themeId);
+}
